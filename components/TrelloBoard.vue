150f4d4f4cb1f1706a1f7fee307b6d0bee407deb
ElvinKyungu <elvinkyungu.75@gmail.com>
2023-09-12 02:52:16 +0200

    add: adding the draggable component TrellBoard

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -1,21 +1,7 @@
-<template>
-    <div class="flex gap-4 overflow-x-auto items-start">
-        <div>
-            <div v-for="column in columns" :key="column.id">
-                <header>
-                    {{ column.title }}
-                </header>
-                <p v-for="task in column.tasks" :key="task.id">
-                    {{ task.title }}
-                </p>
-            </div>
-        </div>
-    </div>
-</template>
-
 <script setup lang="ts">
 import type { Column } from '~~/types'
 import { nanoid } from 'nanoid'
+import draggable from 'vuedraggable';
 
 const columns = ref<Column[]>([
     {
@@ -45,3 +31,33 @@ const columns = ref<Column[]>([
     { title: "Complete", id: nanoid(), tasks: [] }
 ]);
 </script>
+
+<template>
+    <div>
+        <div >
+            <draggable
+                v-model="columns"
+                group-key="columns"
+                item-key="id"
+                class="flex gap-4 overflow-x-auto items-start"
+            >
+                <template #item="{element: column } : {element: Column}">
+                    <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
+                        <header class="font-bold mb-4">
+                            {{ column.title }}
+                        </header>
+                        <TrelloBoardTask 
+                            v-for="task in column.tasks"
+                            :key="task.id"
+                            :task="task"
+                        />
+                        <footer>
+                            <button class="text-gray-500">+ Add a card</button>
+                        </footer>
+                    </div>
+                </template>
+            </draggable>
+        </div>
+    </div>
+</template>
+
---
+++ components/TrelloBoardTask.vue
@@ -0,0 +1,14 @@
+<template>
+    <div :title="task.createdAt.toLocaleDateString()"
+    class="task bg-white p-2 mb-2 rounded shadow-sm max-width-[250px]">
+        <span>
+            {{ task.title}}
+        </span>
+    </div>
+</template>
+<script setup lang="ts">
+import type { Task } from '~~/types'
+defineProps<{
+    task: Task;
+}>();
+</script>
