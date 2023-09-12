a22e389d308aaffc9a335483bfabf0533d3bc091
ElvinKyungu <elvinkyungu.75@gmail.com>
2023-09-12 03:12:42 +0200

    feat : update the animation and the handle draggable

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -39,11 +39,18 @@ const columns = ref<Column[]>([
                 v-model="columns"
                 group-key="columns"
                 item-key="id"
+                animation="150"
+                handle=".draggble"
                 class="flex gap-4 overflow-x-auto items-start"
             >
                 <template #item="{element: column } : {element: Column}">
                     <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
-                        <header class="font-bold mb-4">
+                        <header class="font-bold mb-4 flex items-center gap-2">
+                            <span class="draggble cursor-move">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grip-vertical" viewBox="0 0 16 16">
+                                    <path d="M7 2a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM7 5a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM7 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-3 3a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-3 3a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                                </svg>
+                            </span>
                             {{ column.title }}
                         </header>
                         <TrelloBoardTask 
